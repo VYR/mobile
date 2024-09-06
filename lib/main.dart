fd47c679151439f031ca220380adfb0f468c6f03
tejaswi <tej.tejaswi115@gmail.com>
2024-09-06 15:42:09 +0530

    live rates website added

--- lib/main.dart
+++ lib/main.dart
@@ -24,8 +24,26 @@ class _WebViewAppState extends State<WebViewApp> {
   void initState() {
     super.initState();
     controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onHttpError: (HttpResponseError error) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
       ..loadRequest(
-        Uri.parse('https://flutter.dev'),
+        Uri.parse('https://www.vibullion.com/mobile-app'),
       );
   }
 
@@ -33,7 +51,8 @@ class _WebViewAppState extends State<WebViewApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter WebView'),
+        toolbarHeight: 0,
+        backgroundColor: const Color.fromRGBO(199, 162, 80, 1)
       ),
       body: WebViewWidget(
         controller: controller,
